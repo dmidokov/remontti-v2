43894e43fe8c9b799109c5540583d77d8e5e8e7a
noname <dmidokov@gmail.com>
2022-08-27 17:06:13 +0300

    Добавлена навигация
    -  добавлено  общее место хранения настроек приложения
    - данные для подготовки таблиц вынесены в отдельный файл
    - переделана подготовка бд
    - отдельно создаются таблицы отдельно данные
    - переводы вынесены в БД

---
+++ database/data.go
@@ -0,0 +1,85 @@
+package database
+
+import (
+	"github.com/dmidokov/remontti-v2/config"
+	"github.com/dmidokov/remontti-v2/navigationservice"
+	"github.com/dmidokov/remontti-v2/permissionservice"
+	"github.com/dmidokov/remontti-v2/translationservice"
+	"github.com/dmidokov/remontti-v2/userservice"
+)
+
+type usersList []*userservice.User
+type navigationList []*navigationservice.NavigationItem
+type permissionsList []*permissionservice.Permissons
+type translationsList []*translationservice.Translation
+
+func GetUserDataToInsert(cfg *config.Configuration) usersList {
+
+	return usersList{
+		&userservice.User{CompanyId: 0, UserName: "admin", Password: cfg.ADMIN_PASSWORD, LastLoginDate: 0, LastLoginErrorDate: 0},
+	}
+
+}
+
+func GetNavigationDataToInsert(cfg *config.Configuration) navigationList {
+
+	return navigationList{
+		&navigationservice.NavigationItem{Item_type: 1, Link: "/settings", Label: "Settings", EditTime: 0},
+		&navigationservice.NavigationItem{Item_type: 1, Link: "/logout", Label: "Logout", EditTime: 0},
+	}
+
+}
+
+func (pg *DatabaseModel) GetPermissionsDataToInsert(cfg *config.Configuration) (permissionsList, error) {
+	var userservice userservice.UserModel = userservice.UserModel{DB: pg.DB}
+	var navigationservice navigationservice.NavigationModel = navigationservice.NavigationModel{DB: pg.DB}
+
+	user, err := userservice.GetByNameAndCompanyId("admin", 0)
+	if err != nil {
+		return nil, err
+	}
+
+	items, err := navigationservice.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var permissionList permissionsList = permissionsList{}
+	var actions = permissionservice.Actions
+
+	for _, item := range items {
+		permissionList = append(permissionList, &permissionservice.Permissons{ComponentId: item.Id, UserIid: user.Id, Actions: actions.VIEW | actions.EDIT | actions.DELETE})
+	}
+
+	return permissionList, nil
+}
+
+func GetTranslationsDataToInsert(cfg *config.Configuration) translationsList {
+
+	result := []*translationservice.Translation{}
+
+	var loginpage = translationsList{
+		&translationservice.Translation{Name: "loginpage", Label: "LoginFieldHeader", Ru: "Логин:", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "PasswordFieldHeader", Ru: "Пароль:", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "SignIn", Ru: "Войти", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "LoginFormHeader", Ru: "Вход", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "PageTitle", Ru: "Вход", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "InvalidData", Ru: "Некоректные данные", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "UserIsNotExists", Ru: "Пользователя с таким именем не существует", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "ErrorTryAgain", Ru: "Ошибка, попробуйте еще раз", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "InvalidUserOrPassword", Ru: "Неверный логин или пароль", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "loginpage", Label: "EmptyLoginOrPassword", Ru: "Логин или пароль не указаны", En: "", EditTime: 0},
+	}
+
+	var mainpage = translationsList{
+		&translationservice.Translation{Name: "mainpage", Label: "LoginFieldHeader", Ru: "Меню...", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "mainpage", Label: "AccountSettings", Ru: "Управление аккаунтом", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "mainpage", Label: "Logout", Ru: "Выход", En: "", EditTime: 0},
+		&translationservice.Translation{Name: "mainpage", Label: "Settings", Ru: "Настройки", En: "", EditTime: 0},
+	}
+
+	result = append(result, loginpage...)
+	result = append(result, mainpage...)
+
+	return result
+}
--- database/requests.go
+++ database/requests.go
@@ -1,30 +1,65 @@
 package database
 
-var CreateUsersTable = `
+// Таблицы
+
+var CreateUsersTableSQL = `
 	CREATE TABLE IF NOT EXISTS public.users(
     	id SERIAL PRIMARY KEY NOT NULL,
     	company_id integer NOT NULL,
 		user_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
 		password text COLLATE pg_catalog."default" NOT NULL,
-		last_login_date timestamp without time zone,
-		last_login_error_date timestamp without time zone,
+		last_login_date integer NOT NULL,
+		last_login_error_date integer NOT NULL,
 		CONSTRAINT "User_uniq" UNIQUE (user_name)
 	)
 
-	TABLESPACE pg_default;
+	TABLESPACE pg_default;`
 
-	ALTER TABLE IF EXISTS public.users
-		OWNER to %s;
+var SetUserTableOwnerSql = `ALTER TABLE IF EXISTS public.users OWNER to %s`
 
+var SetUsersTableCommentSQL = `
 	COMMENT ON TABLE public.users
 		IS 'Contains information about users';`
 
-var InsertAdminUser = `
-	INSERT INTO public.users 
-		(company_id, user_name, password) 
-	VALUES 
-		(0, 'admin', '%s');
+var CreateNavigationTableSQL = `
+	CREATE TABLE IF NOT EXISTS public.navigation(
+		id SERIAL PRIMARY KEY NOT NULL,
+		item_type integer NOT NULL,
+		link character varying(100) COLLATE pg_catalog."default" NOT NULL UNIQUE,
+		label character varying(50) COLLATE pg_catalog."default" NOT NULL,
+		edit_time integer
+	)
+
+	TABLESPACE pg_default;
 `
-var SelectAdminCount = `
-	SELECT count(*) FROM public.users WHERE user_name='admin'; 
+
+var CreateTranslationsTableSQL = `
+	CREATE TABLE IF NOT EXISTS public.translations(
+		id SERIAL PRIMARY KEY NOT NULL,
+		name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+		label character varying(30) COLLATE pg_catalog."default" NOT NULL,
+		ru character varying(150) COLLATE pg_catalog."default",
+		en character varying(150) COLLATE pg_catalog."default",
+		edit_time integer
+	)
+
+	TABLESPACE pg_default;
 `
+
+var SetNavigationTableOwnerSQL = `ALTER TABLE IF EXISTS public.navigation OWNER to %s;`
+
+var SetNavigationTableCommentSQL = `COMMENT ON TABLE public.navigation IS 'Contains information about navigation menu';`
+
+var CreatePermissionsTableSQL = `
+	CREATE TABLE IF NOT EXISTS public.permissions(
+		permission_id SERIAL NOT NULL,
+		component_id integer NOT NULL,
+		user_id integer NOT NULL,
+		actions integer NOT NULL,
+		edit_time integer NOT NULL
+	)
+	TABLESPACE pg_default;
+`
+var SetPermissionsTableOwnerSQL = `ALTER TABLE IF EXISTS public.permissions OWNER to %s;`
+
+var SetPermissionsTableCommentSQL = `COMMENT ON TABLE public.permissions IS 'Contains information about user to componens permissions';`
